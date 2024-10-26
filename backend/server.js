b41c44fcc1083a437f51b41f017084a62822456a
Shrey Bhatnagar <121557964+shrey0404@users.noreply.github.com>
2024-10-26 20:45:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,5 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
